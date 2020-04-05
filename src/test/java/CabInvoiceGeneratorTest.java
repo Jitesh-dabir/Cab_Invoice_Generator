a596e55ae2d92375c1a6aa80bcccc74480f0efed
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-06 00:01:19 +0530

    Modified: Pass distance and time as variable in test case

--- src/test/java/CabInvoiceGeneratorTest.java
+++ src/test/java/CabInvoiceGeneratorTest.java
@@ -8,7 +8,9 @@ public class CabInvoiceGeneratorTest {
 
     @Test
     public void givenDistanceAndTime_InvoiceGeneratorGenerateFare_ShouldReturnTotalFareForJourney() {
-        double totalFare = cabInvoiceGenerator.getTotalFare(20, 20);
+        int time = 20;
+        double distance = 20.0;
+        double totalFare = cabInvoiceGenerator.getTotalFare(distance, time);
         Assert.assertEquals(220, totalFare, 0);
     }
 }
