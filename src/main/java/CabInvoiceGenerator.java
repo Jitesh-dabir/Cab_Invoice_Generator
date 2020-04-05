1620101c88ffe50b31300b0bd8d024c6f74fe535
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-05 22:27:32 +0530

    Added: Method to get total fare for journey

--- src/main/java/CabInvoiceGenerator.java
+++ src/main/java/CabInvoiceGenerator.java
@@ -1,5 +1,16 @@
 public class CabInvoiceGenerator {
+
+    //CONSTANTS
+    private static final double COST_PER_KILOMETER = 10;
+    private static final int COST_PER_MINUTE = 1;
+
     public static void main(String[] args) {
         System.out.println("Welcome to cab invoice generator");
+    }
+
+    //METHOD TO GET TOTAL FARE FOR JOURNEY
+    public double getTotalFare(double distanceInKiloMeter, int travelPerMinute) {
+        double totalFare = distanceInKiloMeter * COST_PER_KILOMETER + travelPerMinute * COST_PER_MINUTE;
+        return totalFare;
     }
 }
