08835b5b667dedc7ee873bb9addcb8b59355f94a
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-07 12:12:27 +0530

    Added: Custom exception type in exception class

--- src/main/java/CabInvoiceException.java
+++ src/main/java/CabInvoiceException.java
@@ -1,7 +1,7 @@
 public class CabInvoiceException extends Exception {
 
     public enum MyEXception_Type {
-        NOT_A_VALID_USER
+        NOT_A_VALID_USER, NO_SUCH_RIDE_TYPE;
     }
 
     MyEXception_Type type;
